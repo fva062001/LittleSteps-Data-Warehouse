b377d112cc103b4d56a80472ce930e8a435b5315
Alex <alexserafini2001@gmail.com>
2022-03-26 18:06:13 -0400

    Fixed: mandar a /app en vez de /app.html

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,7 +1,6 @@
 import {Request, Response} from 'express'
 import path from 'path'
 
-export function WelcomeResponse(req: Request, res: Response): Response{
-    res.sendFile(path.join(__dirname+'/index.html'))
-    return res.json("Esta es la API")
+export function WelcomeResponse(req: Request, res: Response){
+    res.sendFile(path.join(__dirname+'../../../dist/app.html'))
 }
